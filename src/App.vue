9203551af0376a57d73975a61917f71360ce049d
Lixquid <lixquid@lixquid.com>
2020-05-11 17:02:53 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,162 @@
+<template lang="pug">
+    .container.my-5.mx-auto
+        h1.mb-5
+            | Debt Resolver
+            |
+            a.btn.btn-outline-primary.float-right(href="https://lixquid.com").
+                lixquid.com
+        .card
+            .card-body
+                .row(v-for="(r, i) in debts", :key="i")
+                    .input-group.col
+                        input.form-control(
+                            type="text"
+                            placeholder="Debtor"
+                            v-model="r.debtor"
+                        )
+                        .input-group-append
+                            button.btn.btn-secondary.dropdown-toggle(data-toggle="dropdown")
+                            .dropdown-menu
+                                a.dropdown-item(
+                                    v-for="(n, i) in allNames"
+                                    :key="i"
+                                    href="#"
+                                    @click.prevent="r.debtor = n"
+                                ) {{n}}
+                    span.debt-text owes
+                    .col-3
+                        input.form-control(
+                            type="number"
+                            v-model.number="r.amount"
+                            min="0"
+                            step="0.01"
+                        )
+                    span.debt-text to
+                    .input-group.col
+                        input.form-control(
+                            type="text"
+                            placeholder="Creditor"
+                            v-model="r.creditor"
+                        )
+                        .input-group-append
+                            button.btn.btn-secondary.dropdown-toggle(data-toggle="dropdown")
+                            .dropdown-menu
+                                a.dropdown-item(
+                                    v-for="(n, i) in allNames"
+                                    :key="i"
+                                    href="#"
+                                    @click.prevent="r.debtor = n"
+                                ) {{n}}
+                    button.btn.btn-outline-danger(
+                        @click.passive="debts.splice(i, 1)"
+                        :disabled="debts.length === 1"
+                    ) #[span.fas.fa-times]
+                button.btn.btn-success.mt-3.w-100(
+                    @click.passive="debts.push({debtor:'', amount: 0, creditor: ''})"
+                ) #[span.fas.fa-plus] New Debt
+            .card-body.border-top
+                ul.mb-0
+                    li(v-if="resolvedDebts.length === 0").
+                        No-one owes anything!
+                    li(
+                        v-for="(d, i) in resolvedDebts"
+                        :key="i"
+                    ) {{d.debtor}} gives {{d.amount}} to {{d.creditor}}
+
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from "@vue/composition-api";
+import { chain } from "lodash";
+interface IDebt {
+    debtor: string;
+    creditor: string;
+    amount: number;
+}
+
+export default defineComponent({
+    setup() {
+        const debts = ref<IDebt[]>([
+            {
+                debtor: "",
+                amount: 0,
+                creditor: ""
+            }
+        ]);
+
+        const allNames = computed(() =>
+            chain(debts.value)
+                .flatMap(d => [d.debtor, d.creditor])
+                .uniq()
+                .filter(v => !!v)
+                .value()
+        );
+
+        const resolvedDebts = computed(() => {
+            // Calculate balance
+            const balances: { [name: string]: number } = {};
+            debts.value.forEach(e => {
+                if (!e.debtor || !e.creditor) {
+                    return;
+                }
+                balances[e.debtor] = balances[e.debtor] ?? 0;
+                balances[e.creditor] = balances[e.creditor] ?? 0;
+                balances[e.debtor] -= e.amount;
+                balances[e.creditor] += e.amount;
+            });
+            console.log(balances);
+
+            // Assign values
+            const transactions: IDebt[] = [];
+
+            Object.keys(balances).forEach(debtor => {
+                if (balances[debtor] >= 0) {
+                    return;
+                }
+                Object.keys(balances).forEach(creditor => {
+                    if (balances[creditor] <= 0) {
+                        return;
+                    }
+                    console.log(debtor, creditor);
+                    if (-balances[debtor] > balances[creditor]) {
+                        // creditor out of credit
+                        transactions.push({
+                            debtor,
+                            creditor,
+                            amount: balances[creditor]
+                        });
+                        balances[debtor] += balances[creditor];
+                        balances[creditor] = 0;
+                    } else {
+                        // debtor debt cleared
+                        transactions.push({
+                            debtor,
+                            creditor,
+                            amount: -balances[debtor]
+                        });
+                        balances[creditor] += balances[debtor];
+                        balances[debtor] = 0;
+                    }
+                    console.log(balances);
+                });
+            });
+
+            return transactions;
+        });
+
+        return {
+            debts,
+            allNames,
+            resolvedDebts
+        };
+    }
+});
+</script>
+
+<style lang="stylus" scoped>
+.debt-text
+    line-height 2
+
+.btn-outline-danger
+    margin-right 15px
+</style>
